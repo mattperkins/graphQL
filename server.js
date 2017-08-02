d10fafdd77dddb65c221e50c6bcdbc6c14d5a3cf
mattperkins <mattperkins@users.noreply.github.com>
2017-08-02 09:47:57 +0100

    schema init

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const expressGraphQL = require('express-graphql')
+const schema = require('./schema.js')
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+schema: schema,
+graphiql: true
+})) 
 
 app.listen(4000, () => {
 console.log('Its time to start running....4000')

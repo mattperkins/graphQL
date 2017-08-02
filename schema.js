9e795e1c6d729a6d93ace35934261355ed704718
mattperkins <mattperkins@users.noreply.github.com>
2017-08-02 23:00:47 +0100

    add delete customer

--- schema.js
+++ schema.js
@@ -60,6 +60,7 @@ return axios.get('http://localhost:3000/customers/' + args.id)
 const mutation = new GraphQLObjectType({
  name: 'Mutation',
  fields: {
+
  addCustomer: {
  type: CustomerType,
  args: {
@@ -68,11 +69,22 @@ const mutation = new GraphQLObjectType({
  age: {type: new GraphQLNonNull(GraphQLInt)},
  },
  resolve(parentValue, args){
- return axios.post('http://localhost:3000/customers', {
+ return axios.post('http://localhost:3000/customers/', {
  name: args.name,
  email: args.email,
  age: args.age
  })
+ .then(res => res.data)
+ }
+ },
+
+ deleteCustomer: {
+ type: CustomerType,
+ args: {
+ id: {type: new GraphQLNonNull(GraphQLString)}
+ },
+ resolve(parentValue, args){
+ return axios.delete('http://localhost:3000/customers/'+args.id)
  .then(res => res.data)
  }
  }

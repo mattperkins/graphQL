19b1bd9103ac85da0cab02d0c64a87d6eba86781
mattperkins <mattperkins@users.noreply.github.com>
2017-08-02 10:16:03 +0100

    add test data to schema

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # npm run s = nodemon server.js
+
+# http://localhost:4000/graphql
--- schema.js
+++ schema.js
@@ -6,3 +6,43 @@ GraphQLSchema,
 GraphQLList,
 GraphQLNonNull
 } = require('graphql');
+
+// Hardcoded data in lieu of a Database call
+const customer = [
+{id: '1', name: 'Sandy', email: 'sandy@email.com', age:19},
+{id: '1', name: 'Max', email: 'max@email.com', age:30},
+{id: '1', name: 'Fred', email: 'fred@email.com', age:44},
+]
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+name: 'Customer',
+fields: () => ({
+id: {type: GraphQLString},
+name: {type: GraphQLString},
+email: {type: GraphQLString},
+age: {type: GraphQLInt},
+})
+})
+// Root Query
+const RootQuery = new GraphQLObjectType({
+name: 'RootQueryType',
+fields: {
+ customer: {
+ type: CustomerType,
+ args: {
+ id: {type: GraphQLString}
+ },
+ resolve(parentValue, args){
+ for (let i=0; i < customers.length; i++){
+ if(customers[i].id == args.id){
+ return customers[i]
+ }	
+ }
+ }
+ } 
+}
+})
+
+module.exports =  new GraphQLSchema({
+query: RootQuery
+})
